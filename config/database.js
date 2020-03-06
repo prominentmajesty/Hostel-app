3c8b9e98ba57d7b44f62c006e29c402ddd68e31f
MAJESTY <ugochukwuaugustine7@gmail.com>
2020-03-06 04:26:53 -0800

    anotther commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/hostelapp',
+    database: 'mongodb+srv://majesty:majesty@hostel-app-2mrhy.mongodb.net/test?retryWrites=true&w=majority',
     // database: 'mongodb://barman:VICEcity47@ds141972.mlab.com:41972/elearning',
     secret: 'barman secret'
 };
